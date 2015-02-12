5590ee37b4d2a71cd8bf2b3222ef19887d00391f
Casey Megginson <tenncasey@gmail.com>
2015-02-12 14:54:03 -0600

    Testing setup with image processing

--- src/cameraocr.cpp
+++ src/cameraocr.cpp
@@ -6,23 +6,34 @@ int main(int argc, char** argv) {
     if(!cap.isOpened()) {
         return -1;
     }
-    cv::Mat im;
-    cap >> im;
-    if (im.empty()) {
-        std::cout << "Cannot open source image!" << std::endl;
-        return -1;
+
+    while(1==1) {
+        cv::Mat im;
+        cap >> im;
+        if (im.empty()) {
+            std::cout << "Cannot open source image!" << std::endl;
+            return -1;
+        }
+
+        cv::Mat contrast;
+        im.convertTo(contrast, -1, 2, 0);
+        //cv::imwrite("output.jpg", contrast);
+
+        cv::Mat gray;
+        cv::cvtColor(contrast, gray, CV_BGR2GRAY);
+
+        cv::Mat blur;
+        cv::GaussianBlur(gray, blur, cv::Size(35.0, 35.0), cv::BORDER_DEFAULT);
+
+        cv::imwrite("output.jpg", blur);
+
+        tesseract::TessBaseAPI tess;
+        tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
+        tess.SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
+        tess.SetImage((uchar*)blur.data, blur.cols, blur.rows, 1, blur.cols);
+
+        char* out = tess.GetUTF8Text();
+        std::cout << out << std::endl;
     }
-
-    cv::Mat gray;
-    cv::cvtColor(im, gray, CV_BGR2GRAY);
-
-    tesseract::TessBaseAPI tess;
-    tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
-    tess.SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
-    tess.SetImage((uchar*)gray.data, gray.cols, gray.rows, 1, gray.cols);
-
-    char* out = tess.GetUTF8Text();
-    std::cout << out << std::endl;
-
     return 0;
 }
