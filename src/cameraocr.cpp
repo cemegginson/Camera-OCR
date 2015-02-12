79036469b17b2f928d357a7818b16c002f008be3
Casey Megginson <tenncasey@gmail.com>
2015-02-11 22:20:40 -0600

    Now reads text from webcam

--- src/cameraocr.cpp
+++ src/cameraocr.cpp
@@ -1,35 +1,28 @@
 #include "cameraocr.h"
-// tess.cpp:
-// Recognize text on an image using Tesseract API and print it to the screen
-// Usage: ./tess image.png
 
-int main (int argc, char** argv) {
-    if (argc != 2) {
-        std::cout << "Please specify the input image!" << std::endl;
+int main(int argc, char** argv) {
+
+    cv::VideoCapture cap(0);
+    if(!cap.isOpened()) {
+        return -1;
+    }
+    cv::Mat im;
+    cap >> im;
+    if (im.empty()) {
+        std::cout << "Cannot open source image!" << std::endl;
         return -1;
     }
 
-    const char* lang = "eng";
-    const char* filename = argv[1];
+    cv::Mat gray;
+    cv::cvtColor(im, gray, CV_BGR2GRAY);
 
     tesseract::TessBaseAPI tess;
-    tess.Init(NULL, lang, tesseract::OEM_DEFAULT);
+    tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
     tess.SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
+    tess.SetImage((uchar*)gray.data, gray.cols, gray.rows, 1, gray.cols);
 
-    FILE* fin = fopen(filename, "rb");
-    if (fin == NULL) {
-        std::cout << "Cannot open " << filename << std::endl;
-        return -1;
-    }
-    fclose(fin);
-
-    STRING text;
-    if (!tess.ProcessPages(filename, NULL, 0, &text)) {
-        std::cout << "Error during processing." << std::endl;
-        return -1;
-    }else{
-        std::cout << text.string() << std::endl;
-    }
+    char* out = tess.GetUTF8Text();
+    std::cout << out << std::endl;
 
     return 0;
 }
